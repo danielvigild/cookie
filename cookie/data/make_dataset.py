356f8f715c152d93c22cec3f0be3310a0451509c
danielvigild <danielvigild>
2024-01-03 20:50:36 +0100

    adding three .py files that kind of work

--- cookie/data/make_dataset.py
+++ cookie/data/make_dataset.py
@@ -1,3 +1,46 @@
+import torch
+import numpy
+
 if __name__ == '__main__':
     # Get the data and process it
+
+    def mnist():
+        """Return train and test dataloaders for MNIST."""
+        # exchange with the corrupted mnist dataset
+        path = "/mnt/c/Users/djv/RFF/MLOps/cookie/data/raw/"
+        train_images, train_labels = [], []
+        for i in range(1, 6):
+            train_images.append(torch.load(path + "train_images_" + str(i) + ".pt"))
+            train_labels.append(torch.load(path + "train_target_" + str(i) + ".pt"))
+
+        test_images = torch.load(path + "test_images.pt")
+        test_labels = torch.load(path + "test_target.pt")
+        
+        # stack the tensors
+        train_images = torch.cat(train_images)
+        train_labels = torch.cat(train_labels)
+        
+        # normalize the images with mean 0 and std 1
+        mean = train_images.mean()
+        std = train_images.std()
+        for i in range(len(train_images)):
+            train_images[i] = (train_images[i] - mean) / std
+
+        # convert to torch tensors
+        train = torch.utils.data.TensorDataset(train_images, train_labels)
+        test = torch.utils.data.TensorDataset(test_images, test_labels)
+
+        # create dataloaders
+        train = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True)
+        test = torch.utils.data.DataLoader(test, batch_size=32, shuffle=False)
+
+        return train, test
+
+    train, test = mnist()
+
+    # save the training and test data to processed
+    torch.save(train, "/mnt/c/Users/djv/RFF/MLOps/cookie/data/processed/train.pt")
+    torch.save(test, "/mnt/c/Users/djv/RFF/MLOps/cookie/data/processed/test.pt")
+
+
     pass
--- cookie/models/model.py
+++ cookie/models/model.py
@@ -1,26 +1,20 @@
-import torch
+from torch import nn
+import torch.nn.functional as F
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
-        
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
 
-        Returns:
-            Output tensor with shape [N,out_features]
+class MyAwesomeModel(nn.Module):
+    """My awesome model."""
 
-        """
-        return self.l2(self.r(self.l1(x)))
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = F.log_softmax(self.fc3(x), dim=1)
+
+        return x
